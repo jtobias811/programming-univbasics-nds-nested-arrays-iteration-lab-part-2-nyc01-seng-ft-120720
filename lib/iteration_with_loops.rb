3a4dbc4c49ade17560c90daf8bd67fc8ef967ee4
Jacob Tobias <jtobias811@gmail.com>
2020-11-04 18:12:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    longest_string_element = ""
+    longest_string_element = 100
     while element_index < src[row_index].count do
       if src[row_index][element_index] > longest_string_element
         longest_string_element = src[row_index][element_index]
